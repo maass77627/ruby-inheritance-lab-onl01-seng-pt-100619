683a6efe690306c13f2fe2b9d2a0bf3cee8259b9
kelly r maass <kmaass77627@gmail.com>
2019-12-14 04:14:08 +0000

    Automatically backed up by Learn

---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+class User 
+  
+  attr_accessor :first_name, :last_name, :teacher, :student 
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end 
+  
+end 
+    
